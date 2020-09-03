9dfcbccef00df4cf35c40f5d295fc4cee8f67de9
zaifrun <martinknud@gmail.com>
2020-09-03 11:27:13 +0200

    added log

--- app/src/main/java/org/pondar/linearlayoutdemokotlin/MainActivity.kt
+++ app/src/main/java/org/pondar/linearlayoutdemokotlin/MainActivity.kt
@@ -2,11 +2,13 @@ package org.pondar.linearlayoutdemokotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.d("UI", "App ready!")
     }
 }
