0f0082b045433c51b49fc5c0983bf82775f5169e
zaifrun <martinknud@gmail.com>
2020-09-09 07:58:58 +0200

    added log

--- app/src/main/java/org/pondar/linearlayoutdemokotlin/MainActivity.kt
+++ app/src/main/java/org/pondar/linearlayoutdemokotlin/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 
+//the interesting thing about this app is in the layout xml file
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
